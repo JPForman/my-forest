5019fc8390851ddebdd98e2daf53214dba7cc6dd
Joshua Forman <cyborgforman@gmail.com>
2020-03-06 09:37:06 -0800

    add broken code to attempt to fix sum scope

--- src/components/TallySheet.jsx
+++ src/components/TallySheet.jsx
@@ -4,27 +4,33 @@ import './TallySheet.css';
 
 function TallySheet(habitEventList){
 
-const [treeTotal, setTreeTotal] = useState(0)
+  const [treeTotal, setTreeTotal] = useState(tempSum)
 
   function SumTree(habitEventList) {
-    console.log('totalTree before', treeTotal);
+    console.log('habitEventList in SumTree', habitEventList);
     console.log('habitEventList.habitEventList in SumTree', habitEventList.habitEventList);
-  }
-    // let treeSet = habitEventList.habitEventList.map(function (habit) { return habit.tree; });
-    // console.log(treeSet);
-    // let sum  = 0
-    // let treeArray = habitEventList.havitEventList
-    // }
+    let treeBools = habitEventList.habitEventList.map(function (habit) {
+      return habit.tree; });
+    console.log('treeBools: ', treeBools);
+    let tempSum = (treeBools.filter(a => a !== false)).length;
+    console.log('tempSum: ', tempSum);
+    // setTreeTotal(tempSum);
+    return tempSum;
+    }
 
+    console.log('totalTree before sum', treeTotal);
     SumTree(habitEventList);
+    console.log('totalTree after sum', treeTotal);
 
-    return(
+    return (
       <div>
         <h1>TallySheet</h1>
         <h3>Total Trees:`${treeTotal}`</h3>
-      
+
       </div>
     );
+
+
   }
 
   export default TallySheet;
