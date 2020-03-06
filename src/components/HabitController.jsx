bf6f57d857566704caecd9f61e37115e50becb38
Joshua Forman <cyborgforman@gmail.com>
2020-03-06 11:08:16 -0800

    add a semi functioning calendar display button

--- src/components/HabitController.jsx
+++ src/components/HabitController.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import './HabitController.css';
+import './HabitController.css';
 import { Button, Modal, ModalHeader, ModalBody, ButtonGroup, ModalFooter, Form } from 'reactstrap';
 import CalendarReact from './CalendarReact';
 import YearForest from './YearForest';
@@ -9,10 +9,10 @@ import TallySheet from './TallySheet';
 
 function HabitController(props){
 
-// Master Habit Event List
-const [habitEventList, setHabitEventList] = useState([]);
+  // Master Habit Event List
+  const [habitEventList, setHabitEventList] = useState([]);
 
-// Modal Stuff
+  // Modal Stuff
   const [modal, setModal] = useState(false);
   const [dateTitle, setDateTitle] = useState('');
 
@@ -20,12 +20,12 @@ const [habitEventList, setHabitEventList] = useState([]);
     let datePartial = value.toString().slice(0,15)
     setDateTitle(`${datePartial}`);
     setModal(!modal);
-   }
+  }
 
-// Checkbox Form Stuff
- const [cSelected, setCSelected] = useState([]);
+  // Checkbox Form Stuff
+  const [cSelected, setCSelected] = useState([]);
 
- const onCheckboxBtnClick = (selected) => {
+  const onCheckboxBtnClick = (selected) => {
     const index = cSelected.indexOf(selected);
     if (index < 0) {
       cSelected.push(selected);
@@ -35,24 +35,21 @@ const [habitEventList, setHabitEventList] = useState([]);
     setCSelected([...cSelected]);
   }
 
-// Submit new Habit to List
-
-
-    //  TRYING TO FIGURE OUT DUPLICATE DATES
-    // if(habitEventList.find(x=>x.date === dateTitle)) {
-    //   console.log('this date exists!');
-    //   console.log('before slice habitEventList', habitEventList);
-    //   let repeatDateIndex = habitEventList.findIndex(x=>x.date === dateTitle)
-    //   let slicedList = habitEventList.slice(repeatDateIndex, repeatDateIndex+1)
-    //   let newHabit = {
-    //     date: dateTitle,
-    //     tree: cSelected.includes('a tree'),
-    //     flower: cSelected.includes('a flower'),
-    //     grass: cSelected.includes('some grass'),
-    //   }
-    //   setHabitEventList(habitEventList => [slicedList, newHabit]);
-    //   console.log('before slice habitEventList', habitEventList);
-    // }
+  //  TRYING TO FIGURE OUT DUPLICATE DATES
+  // if(habitEventList.find(x=>x.date === dateTitle)) {
+  //   console.log('this date exists!');
+  //   console.log('before slice habitEventList', habitEventList);
+  //   let repeatDateIndex = habitEventList.findIndex(x=>x.date === dateTitle)
+  //   let slicedList = habitEventList.slice(repeatDateIndex, repeatDateIndex+1)
+  //   let newHabit = {
+  //     date: dateTitle,
+  //     tree: cSelected.includes('a tree'),
+  //     flower: cSelected.includes('a flower'),
+  //     grass: cSelected.includes('some grass'),
+  //   }
+  //   setHabitEventList(habitEventList => [slicedList, newHabit]);
+  //   console.log('before slice habitEventList', habitEventList);
+  // }
 
   function handleAddHabitEvent(event) {
     event.preventDefault();
@@ -69,34 +66,52 @@ const [habitEventList, setHabitEventList] = useState([]);
     setCSelected([]);
   }
 
+  const [calendarDisplay, setCalendarDisplay] = useState(true);
 
+  function hideCalendar() {
+    console.log('le click hideCalendar');
+    if (calendarDisplay === true) {
+      console.log('calendarDisplay true');
+      setCalendarDisplay('none'); }
+    else {
+      console.log('calendarDisplay false branch')
+      console.log('calendarDisplay pre set: ', calendarDisplay)
+      setCalendarDisplay(true);
+    }
+    console.log('calendarDisplay post set: ', calendarDisplay)
+  return;
+  }
 
+  console.log('calendarDisplay before return: ', calendarDisplay)
   return (
     <div>
-    <CalendarReact toggle={toggle}/>
-    <Modal isOpen={modal} toggle={toggle} >
-      <ModalHeader toggle={toggle}>{dateTitle}</ModalHeader>
-      <ModalBody>
-        Todaysies
-      </ModalBody>
-      <ModalFooter>
-            <Form onSubmit={handleAddHabitEvent}>
-             <ButtonGroup>
-               <Button color="primary" onClick={() => onCheckboxBtnClick('a tree')} active={cSelected.includes(1)}>Add a tree :)</Button>
-               <Button color="primary" onClick={() => onCheckboxBtnClick('a flower')} active={cSelected.includes(2)}>Flower Me</Button>
-               <Button color="primary" onClick={() => onCheckboxBtnClick('some grass')} active={cSelected.includes(3)}>Grass</Button>
-             </ButtonGroup>
-             <button type='submit'>true</button>
-           </Form>
-             <h5>{cSelected[0]}</h5>
+      <div className='calendarReact' style={{display: `${calendarDisplay}`}}>
+        <CalendarReact toggle={toggle}/>
+      </div>
+      <Modal isOpen={modal} toggle={toggle} >
+        <ModalHeader toggle={toggle}>{dateTitle}</ModalHeader>
+        <ModalBody>
+          Todaysies
+        </ModalBody>
+        <ModalFooter>
+          <Form onSubmit={handleAddHabitEvent}>
+            <ButtonGroup>
+              <Button color="primary" onClick={() => onCheckboxBtnClick('a tree')} active={cSelected.includes(1)}>Add a tree :)</Button>
+              <Button color="primary" onClick={() => onCheckboxBtnClick('a flower')} active={cSelected.includes(2)}>Flower Me</Button>
+              <Button color="primary" onClick={() => onCheckboxBtnClick('some grass')} active={cSelected.includes(3)}>Grass</Button>
+            </ButtonGroup>
+            <button type='submit'>true</button>
+          </Form>
+          <h5>{cSelected[0]}</h5>
 
-             <h5>{cSelected[1]}</h5>
+          <h5>{cSelected[1]}</h5>
 
-             <h5>{cSelected[2]}</h5>
-      </ModalFooter>
-    </Modal>
-    <YearForest habitEventList={habitEventList}/>
-    <TallySheet habitEventList={habitEventList}/>
+          <h5>{cSelected[2]}</h5>
+        </ModalFooter>
+      </Modal>
+      <YearForest habitEventList={habitEventList}/>
+      <TallySheet habitEventList={habitEventList}/>
+      <button onClick={hideCalendar}>click</button>
     </div>
   );
 }
--- src/components/TallySheet.jsx
+++ src/components/TallySheet.jsx
@@ -4,28 +4,30 @@ import './TallySheet.css';
 
 function TallySheet(habitEventList){
 
-  const [treeTotal, setTreeTotal] = useState(tempSum)
+  const [treeTotal, setTreeTotal] = useState(0)
+
+  SumTree(habitEventList);
 
   function SumTree(habitEventList) {
     console.log('habitEventList in SumTree', habitEventList);
-    console.log('habitEventList.habitEventList in SumTree', habitEventList.habitEventList);
-    let treeBools = habitEventList.habitEventList.map(function (habit) {
-      return habit.tree; });
-    console.log('treeBools: ', treeBools);
-    let tempSum = (treeBools.filter(a => a !== false)).length;
-    console.log('tempSum: ', tempSum);
-    // setTreeTotal(tempSum);
-    return tempSum;
+    // console.log('habitEventList.habitEventList in SumTree', habitEventList.habitEventList);
+    // let treeBools = habitEventList.habitEventList.map(function (habit) {
+    //   return habit.tree; });
+    // console.log('treeBools: ', treeBools);
+    // let tempSum = (treeBools.filter(a => a !== false)).length;
+    // console.log('tempSum: ', tempSum);
+    // setTreeTotal(1);
+    // return tempSum;
     }
-
-    console.log('totalTree before sum', treeTotal);
-    SumTree(habitEventList);
-    console.log('totalTree after sum', treeTotal);
+    //
+    // console.log('totalTree before sum', treeTotal);
+    // SumTree(habitEventList);
+    // console.log('totalTree after sum', treeTotal);
 
     return (
       <div>
         <h1>TallySheet</h1>
-        <h3>Total Trees:`${treeTotal}`</h3>
+        <h3>Total Trees:`${ treeTotal }`</h3>
 
       </div>
     );
