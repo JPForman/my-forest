1cd787c7834753895ec47f8888f829cda9976f80
Joshua Forman <cyborgforman@gmail.com>
2020-05-13 13:29:16 -0700

    add inputs and submit button to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import './login.css';
+import React, { useState } from 'react';
+import { Form, Input, } from 'reactstrap';
+import './Login.css';
 
 
 function Login(){
+
+  const [givenLoginID, setGivenLoginID] = useState('');
+  const [givenLoginPassword, setGivenLoginPassword] = useState('');
+
+  function checkLogin(givenLoginID, givenLoginPassword){
+    //does a check
+
+  }
+
+  
   return(
     <div>
       welcome to myForest
-      
+      <Form onSubmit={checkLogin}>
+        <Input
+          type="text"
+          className='givenLoginID'
+          name="givenLoginID"
+          onChange={e=> setGivenLoginID(e.target.value)}
+          placeholder="Login ID" />
+        <Input
+          type="text"
+          className='givenLoginPassword'
+          name="givenLoginPassword"
+          onChange={e=> setGivenLoginPassword(e.target.value)}
+          placeholder="Login ID" />
+        <button className='submitLogin' type='submit' value='submit'>submit</button>
+      </Form>
     </div>
   );
 }
