45881473fa55f8891c31d8feb3189301e3cb77d4
Joshua Forman <cyborgforman@gmail.com>
2020-05-13 14:21:54 -0700

    style login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Input, } from 'reactstrap';
 import './Login.css';
+import titleImg from './images/titleImg.png';
 
 
 function Login({setHabitControllerDisplay, setLoginDisplay}){
@@ -16,23 +17,25 @@ function Login({setHabitControllerDisplay, setLoginDisplay}){
 
   
   return(
-    <div>
-      welcome to myForest
-      <Form onSubmit={checkLogin}>
-        <Input
-          type="text"
-          className='givenLoginID'
-          name="givenLoginID"
-          onChange={e=> setGivenLoginID(e.target.value)}
-          placeholder="Login ID" />
-        <Input
-          type="text"
-          className='givenLoginPassword'
-          name="givenLoginPassword"
-          onChange={e=> setGivenLoginPassword(e.target.value)}
-          placeholder="Login ID" />
-        <button className='submitLogin' type='submit' value='submit'>submit</button>
-      </Form>
+    <div className='loginWrapper'>
+      <div className='login'>
+        <img src={titleImg} className='titleImg'></img>
+        <Form onSubmit={checkLogin}>
+          <Input
+            type="text"
+            className='givenLoginID'
+            name="givenLoginID"
+            onChange={e=> setGivenLoginID(e.target.value)}
+            placeholder="Login ID" />
+          <Input
+            type="text"
+            className='givenLoginPassword'
+            name="givenLoginPassword"
+            onChange={e=> setGivenLoginPassword(e.target.value)}
+            placeholder="Login ID" />
+          <button className='submitLogin' type='submit' value='submit'>submit</button>
+        </Form>
+      </div>
     </div>
   );
 }
